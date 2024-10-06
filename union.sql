26ff49d83fe495b971939178708dc6b2ed1607a4
Дмитрий Рычков <80648679+Prospero03@users.noreply.github.com>
2024-10-06 20:57:15 +0800

    Update union.sql

--- union.sql
+++ union.sql
@@ -1,3 +1,5 @@
 SELECT first_name, last_name FROM customer
 UNION
 SELECT first_name, last_name FROM actor;
+
+//Оператор UNION позволяет обединить две однотипных выборки. Эти выборки могут быть из разных таблиц или из одной и той же таблицы. Формальный синтаксис объединения:
